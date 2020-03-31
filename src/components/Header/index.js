d10d74e3b2e991ba7c96504f5d8ed14b21f57ac8
GBorges-ncode <gilbertbdev@gmail.com>
2020-03-30 22:21:42 -0300

    First Commit: Added Pages/Serv/Store/Components

---
+++ src/components/Header/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import { MdShoppingBasket } from 'react-icons/md';
+
+import { Container, Cart } from './styles';
+
+import logo from '../../assets/images/logo.svg';
+
+export default function Header() {
+  const cartSize = useSelector((state) => state.cart.length);
+
+  return (
+    <Container>
+      <Link to="/">
+        <img src={logo} alt="RocketShoes" />
+      </Link>
+
+      <Cart to="/cart">
+        <div>
+          <strong>My Cart</strong>
+          <span>{cartSize} Items</span>
+        </div>
+        <MdShoppingBasket size={36} color="#FFF" />
+      </Cart>
+    </Container>
+  );
+}
+
+// export default connect((state) => ({
+//   // the .cart is the name of the reducer that we can fin in the rootReducer file
+//   cartSize: state.cart.length,
+// }))(Header);
